b53b7b10dfd4b3a93aaa14d3a0cc7729c27ea234
Suvi Heittola <suvi.heittola@helsinki.fi>
2017-03-02 11:53:33 +0200

    Siistimistä

--- src/main/java/tikape/runko/Main.java
+++ src/main/java/tikape/runko/Main.java
@@ -11,7 +11,6 @@ import tikape.runko.database.AihealueDao;
 import tikape.runko.database.Database;
 import tikape.runko.database.KeskusteluDao;
 import tikape.runko.database.KeskustelunavausDao;
-import tikape.runko.database.OpiskelijaDao;
 import tikape.runko.domain.Keskustelu;
 import tikape.runko.domain.Keskustelunavaus;
 
@@ -53,7 +52,6 @@ public class Main {
             return "ok";
         });
 
-        //Listaa keskustelunavaukset aiheittain
         Spark.get("/aihealueet/:id", (req, res) -> {
 
             HashMap<String, Object> data = new HashMap<>();
@@ -87,37 +85,5 @@ public class Main {
             res.redirect("/keskustelu/" + req.params(":id"));
             return "ok";
         });
-
-//        System.out.println(aihealueDao.findOne(1));
-//        System.out.println("");
-//        System.out.println(keskusteluDao.findOne(1));
-//        for (Keskustelu k : keskusteluDao.findAll()) {
-//            System.out.println(k);
-//        }
-//        Database database = new Database("jdbc:sqlite:opiskelijat.db");
-//        database.init();
-//
-//        OpiskelijaDao opiskelijaDao = new OpiskelijaDao(database);
-//
-//        get("/", (req, res) -> {
-//            HashMap map = new HashMap<>();
-//            map.put("viesti", "tervehdys");
-//
-//            return new ModelAndView(map, "index");
-//        }, new ThymeleafTemplateEngine());
-//
-//        get("/opiskelijat", (req, res) -> {
-//            HashMap map = new HashMap<>();
-//            map.put("opiskelijat", opiskelijaDao.findAll());
-//
-//            return new ModelAndView(map, "opiskelijat");
-//        }, new ThymeleafTemplateEngine());
-//
-//        get("/opiskelijat/:id", (req, res) -> {
-//            HashMap map = new HashMap<>();
-//            map.put("opiskelija", opiskelijaDao.findOne(Integer.parseInt(req.params("id"))));
-//
-//            return new ModelAndView(map, "opiskelija");
-//        }, new ThymeleafTemplateEngine());
     }
 }
